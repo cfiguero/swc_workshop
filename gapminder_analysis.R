c2b18c656488cb496fcf8798573e090d7c86dd09
claudia figueroa-romero <cl8us@yahoo.com>
2017-01-18 14:24:16 -0500

    updated gapminder_analysis.R

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,10 +1,8 @@
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
-gapminder <- read.csv("gapminder-FiveYearData.csv")
+fapwdgapminder <- read.csv("gapminder-FiveYearData.csv")
 #Notes form scw
 #Date: January 17-18, 2017
 #Author: Claudia Figueroa
-#Lunch time
-
 
 #head
 #str
